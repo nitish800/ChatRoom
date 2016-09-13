37b9270ec19549abe3a7ec848c432f3d9ca15c75
Nitish shah <nitishshah700@gmail.com>
2016-09-13 14:06:04 +0530

    signup page

---
+++ signup.php
@@ -0,0 +1,54 @@
+<html>
+<head><title>Chat Room Sign UP</title>
+<style>
+
+h1{
+	text-align:center;
+	color:#ffffff;
+}
+.pa{
+	margin-left:500px;
+}
+.inp_size{
+	width:200px;
+}
+.row1{
+	text-align:center;
+}
+th{
+	color:#ffffff;
+}
+</style>
+</head>
+<body bgcolor="#607d8b">
+<h1>SignUp To Enjoy Free PhP Group Chat</h1>
+<br>
+<br>
+<div class="pa">
+<form id="form1" name="form1" method="post" action="signup_process.php">
+
+<table width="400px"  height="300px"  bgcolor="#b0bec5"  >
+
+
+<tr bgcolor="#263238">
+	<th colspan="2" color="#ffffff">SignUp Form</th>
+	
+</tr>
+<tr>
+	<td class="row1" >Enter Name</td>
+	<td class="row1" ><input type="text" name="name" class="inp_size" /></td>
+</tr>
+<tr>
+	<td class="row1" >Enter UserName</td>
+	<td class="row1" ><input type="text" name="u_name" class="inp_size" /></td>
+</tr>
+<tr>
+	<td colspan="2" align="center"><input type="Submit" name="Submit" value="Sign Up"/><input type="Reset" value="Reset"/></td>
+</tr>
+
+</table>
+</form>
+</div>
+
+</body>
+</html>
